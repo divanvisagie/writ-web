8ee991ba5658f0400f3b573e2e0fc4bb1490bd68
Divan Visagie <visagiedivan@gmail.com>
2016-06-01 19:27:40 +0200

    add login route

--- src/app/writ-web.component.ts
+++ src/app/writ-web.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from './login';
+import { Routes , ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router';
 
 @Component({
   moduleId: module.id,
   selector: 'writ-web-app',
   templateUrl: 'writ-web.component.html',
-  styleUrls: ['writ-web.component.css']
+  styleUrls: ['writ-web.component.css'],
+  directives: [ROUTER_DIRECTIVES],
+  providers: [ROUTER_PROVIDERS]
 })
+@Routes([
+  {path: '/login', component: LoginComponent}
+])
 export class WritWebAppComponent {
   title = 'writ-web works!';
 }
