20dc15e0fc06693e8e2a41d97dd2e9779b237734
Divan Visagie <visagiedivan@gmail.com>
2016-06-11 14:47:43 +0200

    ping is batchy

--- src/app/ping/ping.component.ts
+++ src/app/ping/ping.component.ts
@@ -16,14 +16,20 @@ export class PingComponent implements OnInit {
     private pingService: PingService
   ) {}
 
+  responses: string[] = []
   answer: string = 'press the button'
 
   doPing() {
     console.log('do the ping in component')
     this.pingService.doPing().then(answer => {
       this.answer = answer
+      this.responses.push(answer)
       console.log('got answer in component')
     })
+  }
+
+  clear() {
+    this.responses = []
   }
 
   ngOnInit() {
