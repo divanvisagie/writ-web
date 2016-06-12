4d3b1369a9cc5521c337030201daf5f95a3c9645
Divan Visagie <visagiedivan@gmail.com>
2016-06-12 11:47:03 +0200

    fix injection problem that caused test to fail

--- src/app/writ-web.component.spec.ts
+++ src/app/writ-web.component.spec.ts
@@ -7,9 +7,19 @@ import {
 } from '@angular/core/testing';
 import { WritWebAppComponent } from '../app/writ-web.component';
 
+import { Component, provide } from '@angular/core'
+import { AuthenticationService } from './authentication.service';
+
 beforeEachProviders(() => [WritWebAppComponent]);
 
 describe('App: WritWeb', () => {
+
+  class GeneralMock {}
+
+  beforeEachProviders(() => [
+    provide(AuthenticationService, {useClass: AuthenticationService})
+  ]);
+
   it('should create the app',
       inject([WritWebAppComponent], (app: WritWebAppComponent) => {
     expect(app).toBeTruthy();
